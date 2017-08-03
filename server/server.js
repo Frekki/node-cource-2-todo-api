a950bb35a4a1784f57cc7442a650ec842a621202
Frekki <sobolrip1@gmail.com>
2017-08-03 13:45:53 +0300

    Add POST /users/login/ route

--- server/server.js
+++ server/server.js
@@ -119,6 +119,7 @@ app.patch('/todos/:id', (req, res) => {
   });
 });
 
+// POST /users
 app.post('/users', (req, res) => {
   var body = _.pick(req.body, ['email', 'password'])
   var user = new User(body);
@@ -135,6 +136,21 @@ app.post('/users', (req, res) => {
 app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
 });
+
+app.post('/users/login', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email, body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+// bcrypt.compare(user.password, user.otken, (err, res) => {
+//   console.log(res);
+// });
 
 app.listen(port, () => {
   console.log(`Started up at port ${port}`);
