405a3a76a5632735029d26728a598a7423fe79ae
Frekki <sobolrip1@gmail.com>
2017-08-03 14:24:44 +0300

    Add DELETE /users/me/token route

--- server/server.js
+++ server/server.js
@@ -148,9 +148,14 @@ app.post('/users/login', (req, res) => {
     res.status(400).send();
   });
 });
-// bcrypt.compare(user.password, user.otken, (err, res) => {
-//   console.log(res);
-// });
+
+app.delete('/users/me/token', authenticate, (req, res) => {
+  req.user.removeToken(req.token).then(() => {
+    res.status(200).send();
+  }, () => {
+    res.status(400).send();
+  });
+});
 
 app.listen(port, () => {
   console.log(`Started up at port ${port}`);
